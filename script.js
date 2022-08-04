66a8342e1ed13099763c6d165d3881801ee9426e
emmadavids <davidsek88@gmail.com>
2022-08-04 16:23:05 +0100

    refactoring halfway done

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
 const container = document.querySelector('#container')
-for (i = 0; i < 16; i ++) {
+for (let i = 0; i < 16; i ++) {
     let row = document.createElement("div"); 
         row.className = "row";
         container.appendChild(row);
         
-    for (j=0; j <16; j++) { 
+    for (let j=0; j <16; j++) { 
    let box = document.createElement("div");
         box.className = "box";
         row.appendChild(box);
     }
 }
 
-const button = document.querySelector('button')
-button.addEventListener('click', createNewBoard())
+function addGreen() {
+    document.querySelectorAll('.box').forEach(item => {
+        item.addEventListener('mouseover', event => {
+          item.className = "green";
+          console.log("fired")
+        })
+      })
+    }
+    
 
-document.querySelectorAll('.box').forEach(item => {
-    item.addEventListener('mouseover', event => {
-      item.className = "green";
-      console.log("fired")
-    })
-  })
+document.addEventListener('DOMContentLoaded', addGreen() )
 
 function createNewBoard() {
-    let boxAmnt = prompt("How many boxes would you like to make up the etch-a-sketch?")
-
-    for (i = 0; i < boxAmnt.length; i ++) {
+    let boxAmnt = prompt("How many boxes would you like to make up the etch-a-sketch?");
+    console.log("box amount", boxAmnt);
+    const num = parseInt()
+    container.innerHTML = "";
+    for (let i = 0; i < boxAmnt; i ++) {
         let row = document.createElement("div"); 
             row.className = "row";
             container.appendChild(row);
             
-        for (j=0; boxAmnt.length; j++) { 
+        for (let j=0; j < boxAmnt; j++) { 
        let box = document.createElement("div");
             box.className = "box";
             row.appendChild(box);
         }
     }
+    addGreen()
 }  
+
+// const button = document.querySelector('.button')
+// button.addEventListener('click', createNewBoard())
